8423046243e58193fcd01e548c10e123af3d54dd
Thomas Hall <126712774+wavetecrr@users.noreply.github.com>
2023-03-01 19:47:37 +0000

    Delete lkd-changelog.js

--- lkd-changelog.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("lkd-changelog").onload = LKDChangelog();
-
-function LKDChangelog() {
-    document.getElementById("lkd-changelog").innerHTML = '<h1 style="text-align: center; color: white;">New in Version 2.6:</h1><p>Added English Translation.</p><p>Added Language setting.</p><p>Added Crappy Mario bonus level.</p><p>Bugfixes.</p><h1 style="text-align: center; color: white;">v2.6.1:</h1><p>Added partial Controller support: Move with the left joystick, Jump with A, Slow down with B, Pause with the start button.</p><p>Splash screen is now dark and shorter.</p><p>Bugfixes.</p><h1 style="text-align: center; color: white;">v2.6.1:</h1><p>13 bugfixes!</p><p>Android support added back for this version.</p>'
-}
